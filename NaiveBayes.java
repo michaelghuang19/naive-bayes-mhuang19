5f9f384136e57043b20ecf6fa8340b0110662d96
Kushal Jhunjhunwalla <kushaljh@cs.washington.edu>
2019-01-28 23:44:55 -0800

    Update README.md and remove tagging script

---
+++ NaiveBayes.java
@@ -0,0 +1,78 @@
+// Do not submit with package statements if you are using eclipse.
+// Only use what is provided in the standard libraries.
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class NaiveBayes {
+
+    /*
+     * !! DO NOT CHANGE METHOD HEADER !!
+     * If you change the method header here, our grading script won't
+     * work and you will lose points!
+     * 
+     * Train your Naive Bayes Classifier based on the given training
+     * ham and spam emails.
+     *
+     * Params:
+     *      hams - email files labeled as 'ham'
+     *      spams - email files labeled as 'spam'
+     */
+    public void train(File[] hams, File[] spams) throws IOException {
+        // TODO: remove the exception and add your code here
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    /*
+     * !! DO NOT CHANGE METHOD HEADER !!
+     * If you change the method header here, our grading script won't
+     * work and you will lose points!
+     *
+     * Classify the given unlabeled set of emails. Follow the format in
+     * example_output.txt and output your result to stdout. Note the order
+     * of the emails in the output does NOT matter.
+     * 
+     * Do NOT directly process the file paths, to get the names of the
+     * email files, check out File's getName() function.
+     *
+     * Params:
+     *      emails - unlabeled email files to be classified
+     */
+    public void classify(File[] emails) throws IOException {
+        // TODO: remove the exception and add your code here
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+
+    /*
+     *  Helper Function:
+     *  This function reads in a file and returns a set of all the tokens. 
+     *  It ignores "Subject:" in the subject line.
+     *  
+     *  If the email had the following content:
+     *  
+     *  Subject: Get rid of your student loans
+     *  Hi there ,
+     *  If you work for us , we will give you money
+     *  to repay your student loans . You will be 
+     *  debt free !
+     *  FakePerson_22393
+     *  
+     *  This function would return to you
+     *  ['be', 'student', 'for', 'your', 'rid', 'we', 'of', 'free', 'you', 
+     *   'us', 'Hi', 'give', '!', 'repay', 'will', 'loans', 'work', 
+     *   'FakePerson_22393', ',', '.', 'money', 'Get', 'there', 'to', 'If', 
+     *   'debt', 'You']
+     */
+    public static HashSet<String> tokenSet(File filename) throws IOException {
+        HashSet<String> tokens = new HashSet<String>();
+        Scanner filescan = new Scanner(filename);
+        filescan.next(); // Ignoring "Subject"
+        while(filescan.hasNextLine() && filescan.hasNext()) {
+            tokens.add(filescan.next());
+        }
+        filescan.close();
+        return tokens;
+    }
+}
